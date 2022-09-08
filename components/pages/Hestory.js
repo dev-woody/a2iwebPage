dc8bd1d068a059cc412cbff8981c2b39a627de73
woodoll <w_dol97@icloud.com>
2022-09-08 10:46:54 +0900

    test

--- components/pages/Hestory.js
+++ components/pages/Hestory.js
@@ -13,9 +13,9 @@ export default function Hestory() {
           <svg
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             className="w-5 h-5"
             viewBox="0 0 24 24"
           >
