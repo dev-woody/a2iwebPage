6eb648c68d81f9d7ad778d9fb88b53286f49b450
woodoll <w_dol97@icloud.com>
2022-09-13 11:33:48 +0900

    fixerror

--- components/pages/History.js
+++ components/pages/History.js
@@ -5,7 +5,7 @@ import historyImg from "/public/historyImg.jpg";
 export default function History() {
   const History = ({ years, text, icon }) => {
     return (
-      <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+      <div className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
         <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
           <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
         </div>
