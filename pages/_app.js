3f32b880d99ef9b4bed656c22ce5b90978e4aaa3
woodoll <w_dol97@icloud.com>
2022-10-05 16:18:18 +0900

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import "aos/dist/aos.css";
 
 import { useEffect } from "react";
 import AOS from "aos";
-import Head from "next/head";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
@@ -15,12 +14,10 @@ export default function App({ Component, pageProps }) {
   });
   return (
     <Layout>
-      <Head>
-        <Script
-          type="text/javascript"
-          src="https://unpkg.com/lottie-interactive@latest/dist/lottie-interactive.js"
-        ></Script>
-      </Head>
+      <Script
+        type="text/javascript"
+        src="https://unpkg.com/lottie-interactive@latest/dist/lottie-interactive.js"
+      ></Script>
       <Component {...pageProps}></Component>
     </Layout>
   );
